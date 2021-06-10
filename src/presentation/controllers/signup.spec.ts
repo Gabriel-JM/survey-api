9df08e365cc3189ad74a4213e80e4bdf254d0cbf
Gabriel José <gabriel.josemelo@hotmail.com>
2021-06-10 10:27:07 -0300

    test: ensure SignUpController call EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -93,4 +93,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  it('should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    emailValidatorStub.isValid.mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    sut.handle(httpRequest)
+    expect(emailValidatorStub.isValid).toHaveBeenCalledWith(httpRequest.body.email)
+  })
 })

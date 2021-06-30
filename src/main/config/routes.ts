a73a9c13e45a7d9d638f40b5b08dbadd562ae83a
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-06-30 10:44:56 -0300

    feat: ensure signup routes returns an account on success

---
+++ src/main/config/routes.ts
@@ -0,0 +1,15 @@
+import { Express, Router } from 'express'
+import fs from 'fs'
+
+export default (app: Express) => {
+  const router = Router()
+  app.use('/api', router)
+
+  const routesDir = fs.readdirSync('src/main/routes')
+
+  routesDir
+    .filter(fileName => /.+routes\.ts$/.test(fileName))
+    .forEach(
+      async fileName => (await import(`../routes/${fileName}`)).default(router)
+    )
+}

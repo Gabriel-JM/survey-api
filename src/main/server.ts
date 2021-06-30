585564766c10bde4e84021fb85491b890f4a9941
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-06-30 11:48:37 -0300

    feat: connect to mongobd before init server listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(3400, () => console.log(`Server on http://localhost:${3400}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const { default: app } = await import('./config/app')
+    app.listen(env.port, () => console.log(`Server on http://localhost:${env.port}`))
+  })
+  .catch(console.error)

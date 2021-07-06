aa72417566a5665758513f4df1386c8c000e4579
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-07-06 11:33:13 -0300

    refactor: move sut creation to a factory method

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,41 @@
+import { LogControllerDecorator } from './log'
+
+const httpResponseFixture = {
+  statusCode: 200,
+  body: {
+    name: 'Gabriel'
+  }
+}
+
+function makeSut () {
+  const controllerStub = {
+    handle: jest.fn(async () => {
+      return await Promise.resolve(httpResponseFixture)
+    })
+  }
+
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('Log Decorator', () => {
+  const httpRequest = {
+    body: {
+      email: 'any_email@mail.com',
+      name: 'any_name',
+      password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+  }
+
+  it('should call controller handle with correct value', async () => {
+    const { sut, controllerStub } = makeSut()
+    await sut.handle(httpRequest)
+
+    expect(controllerStub.handle).toHaveBeenCalledWith(httpRequest)
+  })
+})

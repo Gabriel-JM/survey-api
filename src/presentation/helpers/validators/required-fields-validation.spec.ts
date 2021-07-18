a9dc315e7b028fe802264c558859c3a3b3063018
Gabriel José <gabriel.josemelo@hotmail.com>
2021-07-18 14:37:12 -0300

    test: ensure RequiredFieldValidation returns correct values

---
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -0,0 +1,20 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('Required fields Validation', () => {
+  it('should return a MissingParamError if field is empty', () => {
+    const sut = new RequiredFieldValidation('anyField')
+
+    const error = sut.validate({ anyField: undefined })
+
+    expect(error).toEqual(new MissingParamError('anyField'))
+  })
+
+  it('should return undefined if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('anyField')
+
+    const error = sut.validate({ anyField: true })
+
+    expect(error).toBeUndefined()
+  })
+})

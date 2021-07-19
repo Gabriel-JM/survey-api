fe9416da170318a548328271a90a6267e90f10c1
Gabriel José <gabriel.josemelo@hotmail.com>
2021-07-19 09:20:40 -0300

    test: ensure ValidationComposite returns the first error when are more then one

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,25 +1,41 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { ValidationComposite } from './validation-composite'
 
 function makeSut () {
-  const validationStub = {
-    validate: jest.fn(() => undefined) as jest.Mock<Error | undefined>
-  }
+  const validationStubs = [
+    {
+      validate: jest.fn(() => undefined) as jest.Mock<Error | undefined>
+    },
+    {
+      validate: jest.fn(() => undefined) as jest.Mock<Error | undefined>
+    }
+  ]
 
-  const sut = new ValidationComposite([validationStub])
+  const sut = new ValidationComposite(validationStubs)
 
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 
 describe('', () => {
   it('should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    validationStub.validate.mockReturnValueOnce(new Error())
+    const { sut, validationStubs } = makeSut()
+    validationStubs[0].validate.mockReturnValueOnce(new Error())
 
     const error = sut.validate({ field: 'any_value' })
 
     expect(error).toEqual(new Error())
+  })
+
+  it('should return the first error if more then on validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    validationStubs[0].validate.mockReturnValueOnce(new MissingParamError('field'))
+    validationStubs[1].validate.mockReturnValueOnce(new InvalidParamError('field'))
+
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toEqual(new MissingParamError('field'))
   })
 })

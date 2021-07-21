a8855805d21e9584861348bdc047d2fbfa98701b
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-07-21 13:36:02 -0300

    feat: ensure DbAuthenticationUseCase calls HashComparer with correct password

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,13 +1,19 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthenticationUseCase implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth (credentials: AuthenticationModel): Promise<string | null> {
-    await this.loadAccountByEmailRepository.load(credentials.email)
+    const account = await this.loadAccountByEmailRepository.load(credentials.email)
+
+    if (!account) return null
+
+    await this.hashComparer.compare(credentials.password, account.password)
     return null
   }
 }

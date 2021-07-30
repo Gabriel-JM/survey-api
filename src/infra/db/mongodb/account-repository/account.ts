1214a214c6dde22b7cdafe642c248d15d7bb09ae
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-07-30 10:25:48 -0300

    feat: ensure MongoAccountRepository update accessToken on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,11 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/update-access-token-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class MongoAccountRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class MongoAccountRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async loadByEmail (email: string): Promise<AccountModel | null> {
     const accountsCollection = await MongoHelper.getCollection('accounts')
     const account = await accountsCollection.findOne({ email })
@@ -21,5 +22,14 @@ export class MongoAccountRepository implements AddAccountRepository, LoadAccount
     const [account] = operationResult.ops
 
     return MongoHelper.map<AccountModel>(account)
+  }
+
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.updateOne({ _id: id }, {
+      $set: {
+        accessToken: token
+      }
+    })
   }
 }

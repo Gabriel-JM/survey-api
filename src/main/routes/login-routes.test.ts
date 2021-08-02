43c81f59cc95819939b89d4c1eb49f90335435cd
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-08-02 10:30:30 -0300

    refactor: rename signup routes to login routes

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('POST /signup', () => {
+    it('should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Gabriel José',
+          email: 'email@mail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+})

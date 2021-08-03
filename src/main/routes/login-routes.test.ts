fe85c6d5cd3ee7fa31afe67bc1993d299d25d8b9
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-08-03 10:55:35 -0300

    test: login route returns 401 if not account was found

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login, if not found the account', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'email@mail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })

a9967f1f05b414b5c95aaf2c506cee8a93c7bbc9
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-08-17 10:47:52 -0300

    test: ensure AddSurveyController returns 400 if Validation returns an error

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,4 +1,5 @@
 import { AddSurveyController } from './add-survey-controller'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 function makeSut () {
   const validationStub = {
@@ -30,5 +31,14 @@ describe('Add Survey Controller', () => {
     await sut.handle(fakeHttpRequest)
 
     expect(validationStub.validate).toHaveBeenCalledWith(fakeHttpRequest.body)
+  })
+
+  it('should return 400 if validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.validate.mockReturnValueOnce(new Error())
+
+    const response = await sut.handle(fakeHttpRequest)
+
+    expect(response).toEqual(badRequest(new Error()))
   })
 })

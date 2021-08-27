371c262aed6e17301c325d1bd906e0795bb70cda
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-08-26 22:57:47 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct value

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,19 +2,44 @@ import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
 
+const fakeAccount = {
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+}
+
 function makeSut () {
-  const sut = new AuthMiddleware()
+  const loadAccountByTokenStub = {
+    load: jest.fn(() => Promise.resolve(fakeAccount))
+  }
+
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
 
   return {
-    sut
+    sut,
+    loadAccountByTokenStub
   }
 }
 
 describe('Auth Middleware', () => {
+  const fakeRequest = {
+    headers: {
+      'x-access-token': 'any_token'
+    }
+  }
+
   it('should return 403 if no x-access-token exists in headers', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  it('should call LoadAccountByToken with correct value', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    await sut.handle(fakeRequest)
+
+    expect(loadAccountByTokenStub.load).toHaveBeenCalledWith('any_token')
   })
 })

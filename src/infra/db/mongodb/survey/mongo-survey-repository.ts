c028382f40589066dab9c1be36ba8da607b914fb
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-09-09 13:42:28 -0300

    feat: ensure MongoSurveyRepository load all surveys

--- src/infra/db/mongodb/survey/mongo-survey-repository.ts
+++ src/infra/db/mongodb/survey/mongo-survey-repository.ts
@@ -1,10 +1,18 @@
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class MongoSurveyRepository implements AddSurveyRepository {
+export class MongoSurveyRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (survey: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(survey)
+  }
+
+  async loadAll () {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+
+    return surveys
   }
 }

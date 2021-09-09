e967a22f9fd5409d717b8e06ef59e7c2ffb51ee6
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-09-09 10:08:05 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,41 @@
+import { DbLoadSurveysUseCase } from './db-load-surveys'
+
+const fakeSurveys = [{
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+}, {
+  id: 'other_id',
+  question: 'other_question',
+  answers: [{
+    image: 'other_image',
+    answer: 'other_answer'
+  }],
+  date: new Date()
+}]
+
+function makeSut () {
+  const loadSurveysRepositoryStub = {
+    loadAll: jest.fn(() => Promise.resolve(fakeSurveys))
+  }
+
+  const sut = new DbLoadSurveysUseCase(loadSurveysRepositoryStub)
+
+  return {
+    sut,
+    loadSurveysRepositoryStub
+  }
+}
+
+describe('Db load surveys use case', () => {
+  it('should call LoadSurveysRepository with correct values', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    await sut.load()
+
+    expect(loadSurveysRepositoryStub.loadAll).toHaveBeenCalledWith()
+  })
+})

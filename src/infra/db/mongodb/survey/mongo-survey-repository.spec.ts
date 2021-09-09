b10df560e909ef502e2e56d689190701c5bfca99
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-09-09 13:42:52 -0300

    test: ensure MongoSurveyRepository load all surveys

--- src/infra/db/mongodb/survey/mongo-survey-repository.spec.ts
+++ src/infra/db/mongodb/survey/mongo-survey-repository.spec.ts
@@ -2,8 +2,25 @@ import { MongoSurveyRepository } from './mongo-survey-repository'
 
 const insertOneStub = jest.fn()
 
+const fakeSurvey = {
+  _id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+}
+
+const toArrayStub = jest.fn(() => Promise.resolve([fakeSurvey]))
+
 const collectionStub = jest.fn((_name) => ({
-  insertOne: insertOneStub
+  insertOne: insertOneStub,
+  find () {
+    return {
+      toArray: toArrayStub
+    }
+  }
 }))
 
 jest.mock('../helpers/mongo-helper', () => {
@@ -40,12 +57,24 @@ describe('Mongo Survey Repository', () => {
     date: new Date()
   }
 
-  it('should add a survey on success', async () => {
-    const sut = new MongoSurveyRepository()
-    const response = await sut.add(fakeSurveyData)
+  describe('add()', () => {
+    it('should add a survey on success', async () => {
+      const sut = new MongoSurveyRepository()
+      const response = await sut.add(fakeSurveyData)
 
-    expect(collectionStub).toHaveBeenCalledWith('surveys')
-    expect(insertOneStub).toHaveBeenCalledWith(fakeSurveyData)
-    expect(response).toBeUndefined()
+      expect(collectionStub).toHaveBeenCalledWith('surveys')
+      expect(insertOneStub).toHaveBeenCalledWith(fakeSurveyData)
+      expect(response).toBeUndefined()
+    })
+  })
+
+  describe('loadAll()', () => {
+    it('should load all surveys on success', async () => {
+      const sut = new MongoSurveyRepository()
+      const response = await sut.loadAll()
+
+      expect(collectionStub).toHaveBeenCalledWith('surveys')
+      expect(response).toEqual([fakeSurvey])
+    })
   })
 })

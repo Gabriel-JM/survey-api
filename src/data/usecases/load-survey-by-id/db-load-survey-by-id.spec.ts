227b9c589c3a145189c134c45e1a2ed2bda65221
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-09-13 21:07:33 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct values

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,40 @@
+import MockDate from 'mockdate'
+import { DbLoadSurveyByIdUsecase } from './db-load-survey-by-id'
+
+const fakeDate = new Date()
+
+const fakeSurvey = {
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: fakeDate
+}
+
+function makeSut () {
+  const loadSurveyByIdRepositoryStub = {
+    loadById: jest.fn(() => Promise.resolve(fakeSurvey))
+  }
+
+  const sut = new DbLoadSurveyByIdUsecase(loadSurveyByIdRepositoryStub)
+
+  return {
+    sut,
+    loadSurveyByIdRepositoryStub
+  }
+}
+
+describe('Db load survey by id use case', () => {
+  beforeAll(() => MockDate.set(fakeDate))
+
+  afterAll(() => MockDate.reset())
+
+  it('should call LoadSurveyByIdRepository with correct values', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    await sut.loadById('any_id')
+
+    expect(loadSurveyByIdRepositoryStub.loadById).toHaveBeenCalledWith('any_id')
+  })
+})

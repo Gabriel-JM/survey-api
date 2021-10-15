6be443cceef047a8c617ce7a0ff7e7956dbf752d
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-10-15 11:04:20 -0300

    feat: ensure LoadSurveyResultController returns 200 on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,5 +1,6 @@
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel } from '@/domain/models/survey'
 import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 
 export class DbLoadSurveyResultUsecase implements LoadSurveyResult {
@@ -12,15 +13,14 @@ export class DbLoadSurveyResultUsecase implements LoadSurveyResult {
     let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
 
     if (!surveyResult) {
-      const survey = await this.loadSurveyByIdRepository.loadById(surveyId)
+      const survey = await this.loadSurveyByIdRepository.loadById(surveyId) as SurveyModel
 
       surveyResult = {
-        surveyId: survey?.id ?? '',
-        question: survey?.question ?? '',
+        surveyId: survey?.id,
+        question: survey?.question,
         answers: survey?.answers
-          .map(answer => ({ ...answer, count: 0, percent: 0 })) ??
-          [],
-        date: survey?.date ?? new Date()
+          .map(answer => ({ ...answer, count: 0, percent: 0 })),
+        date: survey?.date
       }
     }
 

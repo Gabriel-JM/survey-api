d9933439a5c54fb27c548abe1f7c40de01b0a7c3
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-10-19 10:35:29 -0300

    docs: add load survey result path

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/survey-api',
-  port: process.env.PORT ?? 5050,
+  port: process.env.PORT ?? 5000,
   jwtSecret: process.env.JWT_SECRET ?? 'flkg904oj9'
 } as const

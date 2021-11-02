f5e605e1d6c607ed1c2a4fe9ed8492d2d00ec470
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-11-02 16:15:08 -0300

    feat: add error status code on graphql exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,9 +1,26 @@
 import { Controller } from '@/presentation/protocols'
+import { ApolloError, AuthenticationError, ForbiddenError, UserInputError } from 'apollo-server-errors'
 
 export function adaptResolver (controller: Controller) {
   return async (_parent: any, args: any) => {
     const httpResponse = await controller.handle(args)
 
-    return httpResponse.body
+    switch (httpResponse.statusCode) {
+      case 200:
+      case 204:
+        return httpResponse.body
+
+      case 400:
+        throw new UserInputError(httpResponse.body.message)
+
+      case 401:
+        throw new AuthenticationError(httpResponse.body.message)
+
+      case 403:
+        throw new ForbiddenError(httpResponse.body.message)
+
+      default:
+        throw new ApolloError(httpResponse.body.message)
+    }
   }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,12 +1,45 @@
 import { ApolloServer } from 'apollo-server-express'
+import { GraphQLRequestListener, BaseContext } from 'apollo-server-plugin-base'
 import { Express } from 'express'
+import { GraphQLError } from 'graphql'
 import resolvers from '../graphql/resolvers'
 import typeDefs from '../graphql/type-defs'
+
+const errorsMap = {
+  UserInputError: 400,
+  AuthenticationError: 401,
+  ForbiddenError: 403,
+  unknown: 500
+}
+
+function handleErrors (response: any, errors?: readonly GraphQLError[]) {
+  errors?.forEach(error => {
+    response.data = undefined
+
+    for (const errorName of Object.keys(errorsMap)) {
+      if (checkError(error, errorName)) {
+        response.http.status = errorsMap[errorName]
+        return
+      }
+    }
+
+    response.http.status = errorsMap.unknown
+  })
+}
+
+function checkError (error: GraphQLError, errorName: string) {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
 
 export default (app: Express) => {
   const apolloServer = new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    plugins: [{
+      requestDidStart: () => Promise.resolve({
+        willSendResponse: ({ response, errors }) => Promise.resolve(handleErrors(response, errors))
+      }) as Promise<GraphQLRequestListener<BaseContext>>
+    }]
   })
 
   apolloServer.start()
